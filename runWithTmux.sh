6952483779a2e52fe3da123bf0a9eca2e28448fe
gompopcorn <gom.nakonamesh#gmail.com>
2022-01-24 12:10:47 +0000

    run files with tmux

---
+++ runWithTmux.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# export environment variables from .env file
+export $(xargs < .env)
+
+
+function checkUserPwd()
+{
+    # current user password
+    userPwd=$(cat ./tmp.txt)
+
+    # if NO password is provided
+    if [ "$userPwd" == "" ]; then
+        echo -e "${bold_Red}* User password MUST be provided as first argument.${NC}"
+        echo -e "${bold_Red}Exiting...${NC}"
+        exit
+    fi
+
+    # check correctness of the password
+    echo $userPwd | sudo -S ls
+    if [ $? != 0 ]; then
+        echo -e "${bold_Red}* User password is INCORRECT.${NC}"
+        echo -e "${bold_Red}Exiting...${NC}"
+        exit
+    fi
+
+    # clear contents of the password file - (for security)
+    > ./tmp.txt    
+}
+
+
+function run()
+{
+    echo $userPwd | sudo -S apt-get install tmux
+
+    runCommand=$1
+    sessionName=$2
+
+    tmux new -s $sessionName -d
+    tmux send-keys -t $sessionName $runCommand C-m
+    tmux attach -t $sessionName -d    # shows tmux terminal
+}
+
+
+checkUserPwd
+run
