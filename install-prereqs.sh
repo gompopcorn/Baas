183c8b9a8b331de0006d95b548d50d2004223b0c
gompopcorn <95697875+gompopcorn@users.noreply.github.com>
2022-01-24 12:19:40 +0330

    docker installation bug fixed

--- install-prereqs.sh
+++ install-prereqs.sh
@@ -233,6 +233,7 @@ function installDocker()
     "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
     $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     
+    sudo apt-get update
     sudo apt-get -y install docker-ce docker-ce-cli containerd.io
     
     sudo systemctl enable docker
