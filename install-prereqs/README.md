c47b23bcf6a6b966e5100302bfc18ae2bf564398
gompopcorn <gom.nakonamesh#gmail.com>
2022-01-30 13:34:18 +0000

    building notification system

---
+++ install-prereqs/README.md
@@ -0,0 +1,2 @@
+# Password MUST be provided in the ./pwd.txt file
+## with no more characters before or after that
---
+++ install-prereqs/runWithTmux.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# export environment variables from .env file
+export $(xargs < .env)
+
+
+function checkUserPwd()
+{
+    # current user password
+    userPwd=$(cat ./pwd.txt)
+
+    # if NO password is provided
+    if [ "$userPwd" == "" ]; then
+        echo -e "${bold_Red}* User password MUST be provided in pwd.txt file.${NC}"
+        echo -e "${bold_Red}Exiting...${NC}"
+        exit
+    fi
+
+    # check correctness of the password
+    echo $userPwd | sudo -S ls
+    if [ $? != 0 ]; then
+        echo -e "${bold_Red}* User password is INCORRECT.${NC}"
+        echo -e "${bold_Red}Exiting...${NC}"
+        exit
+    fi 
+}
+
+
+function run()
+{
+    echo $userPwd | sudo -S apt-get install tmux
+
+    runCommand=$1
+    sessionName=$2
+
+    tmux new -s $sessionName -d
+    tmux send-keys -t $sessionName $runCommand C-m
+    tmux attach -t $sessionName -d    # shows tmux terminal
+}
+
+
+checkUserPwd
+run
